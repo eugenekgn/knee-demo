de0b3b690f241f421e717c03221aac0616768e52
Eugene Kagan <eugenekgn@gmail.com>
2017-04-13 09:31:19 -0400

    added chalk errors

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ const knex = require('knex')(knexConfig.sqlite);
 screen.clearScreen();
 knex.select('title', 'rating').from('book').asCallback((err, rows) => {
   if (err) {
-    console.log(err);
+    screen.error(err);
   } else {
     screen.write(rows, 'json')
   }
-
 })
 
 knex.destroy();
--- screen.js
+++ screen.js
@@ -1,6 +1,9 @@
+
+const chalk = require('chalk');
+
 const screen = {
   clearScreen: () => {
-    process.stdout.write('\033c')
+    process.stdout.write('\033c');
   },
   write: (data, mode) => {
 
@@ -9,6 +12,10 @@ const screen = {
       output = JSON.stringify(data, null, 4)
     }
     console.log(output);
+  },
+  error: (errorMessage) => {
+    const messageFormatter = chalk.bold.red;
+    console.log(messageFormatter(errorMessage));
   }
 }
 
